89383c2b4a08b8453418f81a4eb6aca2f0b18de0
Romain Fontaine <romain-fontaine@outlook.com>
2015-01-29 17:05:42 +0100

    [add] fetch selfie media url with tweet id

--- tweetbot.py
+++ tweetbot.py
@@ -1,5 +1,7 @@
 import tweepy, time, sys
- 
+from twitter import OAuth, Twitter
+import os.path
+
 CONSUMER_KEY = 'hXNJGzk3drcOhgonq3zsDZDSL'
 CONSUMER_SECRET = 'kup9SzdPtzzghcEMq4D1kd2npmLRn3h0lNMOyn3FgTOWzjQeHv'
 ACCESS_KEY = '3004088237-GzNZtNGxp2aHgIKWfdau1HZDr5QjxcIbJLUmRDv'
@@ -8,12 +10,34 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+autht = OAuth(ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+t = Twitter(auth=autht)
+mediasfile = "medias.txt"
+if not os.path.isfile(mediasfile):
+	f = open(mediasfile, "w")
+	f.close()
+
 while True:
 	# follow every follower
 	for follower in tweepy.Cursor(api.followers).items():
-	    follower.follow()
+		follower.follow()
 
 	# pick random selfie tweet
+	q = "selfie"
+	tweets = t.search.tweets(q=q, filter="images")
+	medias = []
+
+	for tweet in tweets["statuses"]:
+		media = tweet["entities"].get("media", False)
+		if media:
+			media = map(lambda x:x["media_url"], media)
+			status_id = tweet["id"]
+			if not str(media) in open(mediasfile).read():
+				medias.append((status_id, media))
+				with open(mediasfile, "a") as f:
+					f.write(str(media))
+					f.write("\n")
+
 
 	# moustache said selfie
 
