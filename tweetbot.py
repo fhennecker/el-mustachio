4f8297e7563c9ea9e95f3d8a7395aef09498f82a
haxelion <github@haxelion.eu>
2015-01-29 19:48:10 +0100

    One shot mode.

--- tweetbot.py
+++ tweetbot.py
@@ -2,6 +2,7 @@ import tweepy, time, sys
 #from twitter import OAuth, Twitter
 import os.path
 import elmustachio
+import re
 
 CONSUMER_KEY = 'hXNJGzk3drcOhgonq3zsDZDSL'
 CONSUMER_SECRET = 'kup9SzdPtzzghcEMq4D1kd2npmLRn3h0lNMOyn3FgTOWzjQeHv'
@@ -18,47 +19,71 @@ if not os.path.isfile(mediasfile):
 
 elmustachio.init()
 
-while True:
-    # follow every follower
-    # for follower in tweepy.Cursor(api.followers).items():
-    #    follower.follow()
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+    id_re = re.compile(".*/status/([0-9]+)/.*")
+    tweet_id = id_re.match(url).groups()[0]
+    tweet = api.statuses_lookup([tweet_id])[0]
+    media = tweet.entities.get("media", False)
+    if media:
+        media = map(lambda x:x["media_url"], media)
+        status_id = tweet.id
+        if hasattr(tweet, 'retweeted_status'):
+            user = tweet.retweeted_status.author.screen_name
+        else:
+            user = tweet.author.screen_name
+        downloaded_filename = str(status_id)+".jpg"
+        os.system("curl "+media[0]+" -o "+downloaded_filename)
+        # moustaching it
+        result = elmustachio.goMoustachioGo(downloaded_filename)
+        if result != None:
+            # moustaching succeeded
+            print result
+            api.update_with_media(result, "Muchos Mustachios! @"+user,  in_reply_to_status_id=str(status_id))
 
-    # pick random selfie tweet
-    q = "selfie"
-    tweets = api.search(q=q, count=10, filter="images")
-    medias = []
+else:
 
-    for tweet in tweets:
-        media = tweet.entities.get("media", False)
-        if media:
-            media = map(lambda x:x["media_url"], media)
-            status_id = tweet.id
-            if hasattr(tweet, 'retweeted_status'):
-                user = tweet.retweeted_status.author.screen_name
-            else:
-                user = tweet.author.screen_name
-            safe = not tweet.possibly_sensitive
-            if safe and not str(media) in open(mediasfile).read():
-                medias.append((status_id, safe, media, user))
-                with open(mediasfile, "a") as f:
-                    f.write(str(media))
-                    f.write("\n")
-    print medias
-    moustached = False
-    for media in medias:
-        if not moustached:
-            # downloading media
-            downloaded_filename = str(media[0])+".jpg"
-            os.system("curl "+media[2][0]+" -o "+downloaded_filename)
-            # moustaching it
-            result = elmustachio.goMoustachioGo(downloaded_filename)
-            if result != None:
-                # moustaching succeeded
-                print result
-                api.update_with_media(result, "Muchos Mustachios! @"+media[3],  in_reply_to_status_id=str(media[0]))
-                moustached = True
-            else:
-                os.system("rm "+ downloaded_filename)
+    while True:
+        # follow every follower
+        # for follower in tweepy.Cursor(api.followers).items():
+        #    follower.follow()
 
-    # wait for 10 minutes
-    time.sleep(10*60)
+        # pick random selfie tweet
+        q = "selfie"
+        tweets = api.search(q=q, count=20, filter="images")
+        medias = []
+
+        for tweet in tweets:
+            media = tweet.entities.get("media", False)
+            if media:
+                media = map(lambda x:x["media_url"], media)
+                status_id = tweet.id
+                if hasattr(tweet, 'retweeted_status'):
+                    user = tweet.retweeted_status.author.screen_name
+                else:
+                    user = tweet.author.screen_name
+                safe = not tweet.possibly_sensitive
+                if safe and not str(media) in open(mediasfile).read():
+                    medias.append((status_id, safe, media, user))
+                    with open(mediasfile, "a") as f:
+                        f.write(str(media))
+                        f.write("\n")
+        print medias
+        moustached = False
+        for media in medias:
+            if not moustached:
+                # downloading media
+                downloaded_filename = str(media[0])+".jpg"
+                os.system("curl "+media[2][0]+" -o "+downloaded_filename)
+                # moustaching it
+                result = elmustachio.goMoustachioGo(downloaded_filename)
+                if result != None:
+                    # moustaching succeeded
+                    print result
+                    api.update_with_media(result, "Muchos Mustachios! @"+media[3],  in_reply_to_status_id=str(media[0]))
+                    moustached = True
+                else:
+                    os.system("rm "+ downloaded_filename)
+
+        # wait for 10 minutes
+        time.sleep(10*60)
