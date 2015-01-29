341f6e13d72a16866853a427d7a857fbc92d57c7
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-01-29 16:17:24 +0100

    [add] tweetbot following its followers

---
+++ tweetbot.py
@@ -0,0 +1,15 @@
+import tweepy, time, sys
+ 
+CONSUMER_KEY = 'hXNJGzk3drcOhgonq3zsDZDSL'
+CONSUMER_SECRET = 'kup9SzdPtzzghcEMq4D1kd2npmLRn3h0lNMOyn3FgTOWzjQeHv'
+ACCESS_KEY = '3004088237-GzNZtNGxp2aHgIKWfdau1HZDr5QjxcIbJLUmRDv'
+ACCESS_SECRET = 'ePy8t3F683CfPEmBacQEBU1JARy5oX2SaZPDACoBqMNdZ'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+ 
+#api.update_status(status='The world needs more moustaches')
+
+public_tweets = api.home_timeline()
+for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
