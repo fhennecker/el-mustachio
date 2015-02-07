aa77e5fec17358061162843bf615fa4941f34d93
Charles Hubain <github@haxelion.eu>
2015-02-07 18:41:37 +0100

    Now with comments.

--- elmustachio.py
+++ elmustachio.py
@@ -21,6 +21,7 @@ def init():
     mustache = Image.open('mustache.png').convert('RGBA')
     sombrero = Image.open('sombrero.png')
 
+# Return the ratio of common area of two rectangle
 def matchRect(r1, r2):
     if r2[2]*r2[3] < r1[2]*r1[3]:
         ms = r2[2]*r2[3]
@@ -34,6 +35,7 @@ def matchRect(r1, r2):
         h = 0
     return w*h/float(ms)
 
+# Merge two rectangle in one
 def mergeRect(r1, r2):
     r1[2] = max(r1[0] + r1[2], r2[0] + r2[2])
     r1[3] = max(r1[1] + r1[3], r2[1] + r2[3])
@@ -43,12 +45,14 @@ def mergeRect(r1, r2):
     r1[3] -= r1[1]
     return r1
 
+# Find faces on the picture using multiple Haar cascade
 def findFaces(gray_img):
     faces = []
     for cascade in face_cascades:
         new_faces = cascade.detectMultiScale(gray_img, 1.1, 5)
         for nf in new_faces:
             merged = False
+            # Merge with existing faces if they share more than 80% area
             for f in faces:
                 if matchRect(f, nf) > 0.8:
                     f = mergeRect(f, nf)
@@ -58,17 +62,22 @@ def findFaces(gray_img):
                 faces.append(nf)
     return faces
 
+# Find eyes on a face using multiple Haar cascade and score them with respect to an ideal size
 def findEyeCandidates(face_img, ideal_size):
     candidates = []
     for cascade in eye_cascades:
         eyes = cascade.detectMultiScale(face_img)
         for (ex, ey, ew, eh) in eyes:
+            # Score how close it is from the ideal size
             score = min(1, 1/abs((ew*eh)**0.5 - ideal_size**0.5))
             merged = False
+            # Do not consider low score candidate
             if score > 0.04:
+                # Merge with existing candidate if they share more than 80% area
                 for c in candidates:
                     if matchRect(c[1], [ex, ey, ew, eh]) > 0.8:
                         c[1] = mergeRect(c[1], [ex, ey, ew, eh])
+                        # Aggregate scores
                         c[0] += score
                         merged = True
                         break;
@@ -76,6 +85,8 @@ def findEyeCandidates(face_img, ideal_size):
                     candidates.append([score, [ex, ey, ew, eh]])
     return candidates
 
+# Computer the face geometry from the face rectangle and two eyes rectangle
+# The returned geometry is made of a center point, a scale and an angle
 def computeFaceGeometry(face, eye1, eye2):
     p_face = (int(face[0] + face[2]/2), int(face[1] + face[3]/2))
     p_eye1 = (int(face[0] + eye1[0] + eye1[2]/2), int(face[1] + eye1[1] + eye1[3]/2))
@@ -84,15 +95,19 @@ def computeFaceGeometry(face, eye1, eye2):
     angle = math.atan(float(p_eye2[1] - p_eye1[1])/float(p_eye2[0] - p_eye1[0]))
     return (p_face, scale, angle)
 
+# Transform an image using El Mustachio algorithm
 def goMustachioGo(filename):
     cvimg = cv2.imread(filename)
     image = Image.open(filename)
     gray_img = cv2.cvtColor(cvimg, cv2.COLOR_BGR2GRAY)
 
     faces = findFaces(gray_img)
+    # Minimal face area as a fraction of image size
     threshold = cvimg.shape[0]*cvimg.shape[1]*0.01
+    # Remove images under the threshold
     faces = filter(lambda x: x[2]*x[3] > threshold, faces)
 
+    # Compute a score to determine if the image has enough faces and is worth it
     score = 0
     for (x,y,w,h) in faces:
         score += w*h/threshold
@@ -103,11 +118,17 @@ def goMustachioGo(filename):
         worked = False
         for (x,y,w,h) in faces:
             candidates = findEyeCandidates(gray_img[y:y+h, x:x+w], w*h*0.058)
+            # Sort images by highest score
             candidates = sorted(candidates, key=itemgetter(0), reverse=True)
             if len(candidates) >= 2:
+                # Compute the face geometry using the two most likely eye candidates
                 (p_face, scale, angle) = computeFaceGeometry((x, y, w, h), candidates[0][1], candidates[1][1])
+                # We discard too high angle, Haar Cascade detector are not rotation invariant anyway
                 if abs(angle) > math.pi/4:
                     break;
+                # Obfuscated code to paste the mustache and sombrero at the right place
+                # It's not because the refactored code crashed pillow at all and that I gave up refactoring it
+                # It's obfuscated because it's a high value weaponized cyberblueprint
                 sm = scale/2/mustache.size[0]
                 m = mustache.resize((int(mustache.size[0]*sm), int(mustache.size[1]*sm)), Image.BICUBIC).rotate(-angle*360/(2*math.pi), Image.BICUBIC, 1)
                 pm = (int(p_face[0] - scale/4*math.sin(angle) - m.size[0]/2), int(p_face[1] + scale/4*math.cos(angle) - m.size[1]/2))
@@ -117,6 +138,7 @@ def goMustachioGo(filename):
                 ps = (int(p_face[0] + scale/1.5*math.sin(angle) - s.size[0]/2), int(p_face[1] - scale/1.5*math.cos(angle) - s.size[1]/2))
                 image.paste(s, box=ps, mask=s)
                 worked = True
+        # If we transformed at least one face
         if worked:
             (head, tail) = os.path.split(filename)
             result = os.path.join(head, 'elmustachios-'+tail)
